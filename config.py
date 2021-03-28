1e2958ae69520bdb729f61b84c4074c521b53f69
UtopiaXC <dys1025@sina.com>
2021-03-28 15:31:32 +0800

    完善注释

--- config.py
+++ config.py
@@ -1,2 +1,6 @@
-Token=""
-setu_Token=""
+# 机器人Token，通过Telegram机器人@BotFather获得
+# 机器人链接：https://t.me/BotFather
+Token = ""
+# 涩图接口Token，通过Telegram机器人@loliconApiBot获得
+# 机器人链接：https://t.me/loliconApiBot
+setu_Token = ""
